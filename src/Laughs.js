5ed271ea6a245037fc7d7684284d11a278d34ed2
alechp <me@alechp.com>
2018-07-12 11:56:05 -0700

    Added eg. with laugh state to show that local state isn't used

--- src/Laughs.js
+++ src/Laughs.js
@@ -3,6 +3,12 @@ import connect from "./connect";
 import JokesContainer from "./jokes-container";
 
 class Laughs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      laughList: "laughStateInsideLaughComponentIsntUsed"
+    };
+  }
   componentDidMount() {
     const [jokes] = this.props.containers;
     jokes.getJoke();
