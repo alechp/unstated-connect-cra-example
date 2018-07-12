722b3ffc3911e9868bf8aa2d64161f5040b66b36
alechp <me@alechp.com>
2018-07-12 15:08:14 -0700

    Got rid of ugly array syntax. Finally looked inside unstated's context obj

--- src/Laughs.js
+++ src/Laughs.js
@@ -3,18 +3,29 @@ import connect from "./connect";
 import JokesContainer from "./jokes-container";
 
 class Laughs extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       laughList: "laughStateInsideLaughComponentIsntUsed"
     };
   }
   componentDidMount() {
-    const [jokes] = this.props.containers;
-    jokes.getJoke();
+    // const [jokes] = this.props.containers;
+    const jokes = this.props.containers[0];
+    //This technically works, but the big problem here is that if there's more than one connect...
+    jokes.print("Alec");
+    console.log(`this.props.containers: ${this.props.containers}`);
+    jokes.getJoke(); //no brackets, getJoke is synchronous
   }
   render() {
     const [jokes] = this.props.containers;
+    // let data = jokes.foobar(); //Promise stays pending
+    let data = [jokes.foobar()]; //Promise finishes
+    let data2 = Promise.resolve([jokes.foobar()]); //Promise finishes
+
+    jokes.print("World"); //no brackets, print is sync
+    console.log(data);
+    console.log(data2);
 
     return (
       <div>
@@ -27,3 +38,8 @@ class Laughs extends Component {
 }
 
 export default connect([JokesContainer])(Laughs);
+
+//TODO: See if I can replace everything with one jokes call
+//TODO: See what happens when I try to connect multiple containers...
+//? Does the connection order ever change or persist based on call?
+//? Is it possible to call by key?
--- src/jokes-container.js
+++ src/jokes-container.js
@@ -3,15 +3,20 @@ import { Container } from "unstated";
 export default class JokesContainer extends Container {
   constructor() {
     super();
-
+    console.log(`Jokes container constructed`);
     this.state = {
       jokeList: ["priest", "rabbi", "horse", "bar"],
       laughList: ["ha", "hehe", "ha ha", "hahaha"]
     };
   }
-
+  async foobar() {
+    var url = "https://jsonplaceholder.typicode.com/posts/1";
+    return await fetch(url);
+  }
   getJoke() {
     this.setState({ list: this.state.jokeList.concat("joke") });
-    console.log(this.name);
+  }
+  print(me) {
+    console.log(`Herro, ${me}`);
   }
 }
